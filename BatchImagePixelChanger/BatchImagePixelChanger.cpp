2c49aba82cf42a9a2fbf333289aff95065d2cf3f
Some Random Coder <kazakuri994@gmail.com>
2013-12-21 16:33:13 -0500

    Fixed stupid errors

--- BatchImagePixelChanger/BatchImagePixelChanger.cpp
+++ BatchImagePixelChanger/BatchImagePixelChanger.cpp
@@ -37,27 +37,27 @@ int _tmain(int argc, _TCHAR* argv[])
 		for(int i = 1; i <= numCpy; i++) {
 			//Create a back up of our image that we will modify
 			CImage *img = new CImage();
-			(*img).Create(orig.GetWidth(), orig.GetHeight(), 32);
-			orig.BitBlt((*img).GetDC(), 0, 0);
-			(*img).ReleaseDC();
+			img->Create(orig.GetWidth(), orig.GetHeight(), 32);
+			orig.BitBlt(img->GetDC(), 0, 0);
+			img->ReleaseDC();
 
 			//Get pixel count of our image
-			int pixels = (*img).GetWidth() * (*img).GetHeight();
+			int pixels = img->GetWidth() * img->GetHeight();
 
 			//Replace 5% of the pixels
 			for(int x = 0; x < pixels * 0.05; x++) {
 				//Find a random location
-				int width = (rand()%(*img).GetWidth());
-				int height = (rand()%(*img).GetHeight());
+				int width = (rand()%img->GetWidth());
+				int height = (rand()%img->GetHeight());
 
 				//Get the current color
-				COLORREF color = (*img).GetPixel(width, height);
+				COLORREF color = img->GetPixel(width, height);
 
 				//subtract 5 from the RGB values
 				color = RGB(max(GetRValue(color)-5, 0), max(GetGValue(color)-5, 0), max(GetBValue(color)-5, 0));
 
 				//replace our pixel
-				(*img).SetPixel(width, height, color);
+				img->SetPixel(width, height, color);
 			}
 
 			CString name = "";
@@ -77,16 +77,17 @@ int _tmain(int argc, _TCHAR* argv[])
 
 			//save our file
 			if(ext == ".png")
-				(*img).Save(name + t, Gdiplus::ImageFormatPNG);
+				img->Save(name + t, Gdiplus::ImageFormatPNG);
 			if(ext == ".jpg" || ext == ".jpeg")
-				(*img).Save(name + t, Gdiplus::ImageFormatJPEG);
+				img->Save(name + t, Gdiplus::ImageFormatJPEG);
 			if(ext == ".gif")
-				(*img).Save(name + t, Gdiplus::ImageFormatGIF);
+				img->Save(name + t, Gdiplus::ImageFormatGIF);
 			else
-				(*img).Save(name + t, Gdiplus::ImageFormatPNG);
+				img->Save(name + t, Gdiplus::ImageFormatPNG);
 
 			//free our stored copy
-			free(img);
+			delete img;
+			img = NULL;
 		}
 	}
 	catch(...) {
